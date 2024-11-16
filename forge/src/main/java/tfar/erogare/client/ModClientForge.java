65d2635fcd82dfbc8f2c9553df9eb3389e3c1e93
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-11-16 15:45:39 -0500

    add effects

--- forge/src/main/java/tfar/erogare/client/ModClientForge.java
+++ forge/src/main/java/tfar/erogare/client/ModClientForge.java
@@ -1,18 +1,41 @@
 package tfar.erogare.client;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import net.minecraftforge.client.gui.overlay.VanillaGuiOverlay;
 import net.minecraftforge.eventbus.api.IEventBus;
 import tfar.erogare.Erogare;
-import tfar.erogare.ErogareForge;
+import tfar.erogare.init.ModMobEffects;
 
 public class ModClientForge {
-    protected static final ResourceLocation WATCHED_LOCATION = Erogare.id("textures/misc/watched.png");
+    protected static final ResourceLocation[] WATCHED_LOCATIONS = array("watched");
+    protected static final ResourceLocation[] CORRUPTED_LOCATIONS = array("corrupted");
+
+    static ResourceLocation[] array(String id) {
+        int count = 4;
+        ResourceLocation[] locations = new ResourceLocation[count];
+        for (int i = 0; i < count;i++) {
+            locations[i] = Erogare.id("textures/misc/"+id+"_"+i+".png");
+        }
+        return locations;
+    }
 
     static final IGuiOverlay overlay = (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
-        gui.renderTextureOverlay(guiGraphics,WATCHED_LOCATION,1);
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player!= null) {
+            if (player.hasEffect(ModMobEffects.WATCHED)) {
+                int level = Math.min(player.getEffect(ModMobEffects.WATCHED).getAmplifier(), WATCHED_LOCATIONS.length-1);
+                gui.renderTextureOverlay(guiGraphics, WATCHED_LOCATIONS[level], 1);
+            }
+
+            if (player.hasEffect(ModMobEffects.CORRUPTED)) {
+                int level = Math.min(player.getEffect(ModMobEffects.CORRUPTED).getAmplifier(), CORRUPTED_LOCATIONS.length-1);
+                gui.renderTextureOverlay(guiGraphics,CORRUPTED_LOCATIONS[level], 1);
+            }
+        }
     };
 
     public static void init(IEventBus bus) {
@@ -20,7 +43,7 @@ public class ModClientForge {
     }
 
     static void customOverlay(RegisterGuiOverlaysEvent event) {
-        event.registerAbove(VanillaGuiOverlay.HELMET.id(),"corruption",overlay);
+        event.registerAbove(VanillaGuiOverlay.HELMET.id(),"effect_overlay",overlay);
     }
 
 }
